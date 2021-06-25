3bf77ff8b386024e0e7b62e614cbbc1bd339c4a8
Tturna <eemelijuponaho@gmail.com>
2021-06-26 01:30:45 +0300

    the thing sends midi shit now

--- oscserver.py
+++ oscserver.py
@@ -1,5 +1,13 @@
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.dispatcher import Dispatcher
+from simplecoremidi import send_midi
+from time import sleep
+
+channel = 1
+note_on_action = 0x90
+velocity = 127
+
+note = 60
 
 # Set up an OSC Server to receive data from Wekinator
 def print_handler(address, *args):
@@ -7,6 +15,12 @@ def print_handler(address, *args):
 
 def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
+
+    if ("Circle" in str(address)):
+        print("Sending MIDI...")
+        send_midi((note_on_action | channel, note, velocity))
+        sleep(0.25)
+        send_midi((note_on_action | channel, note, 0))
 
 dispatcher = Dispatcher()
 dispatcher.map("/something/*", print_handler)
