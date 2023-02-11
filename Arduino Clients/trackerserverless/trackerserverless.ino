59662044438f1f692fb47a0f21a507ae89f87090
Tturna <eemelijuponaho@gmail.com>
2023-02-11 19:06:26 +0200

    added serverless tracker

--- Arduino Clients/trackerserverless/trackerserverless.ino
+++ Arduino Clients/trackerserverless/trackerserverless.ino
@@ -2,38 +2,42 @@
 //#undef max
 //#undef min
 //#include <SPI.h>
-#include <WiFiNINA.h>
+//#include <WiFiNINA.h>
+#include <ArduinoOSCWiFi.h>
 #include <MadgwickAHRS.h>
 #include <Math.h>
 //#include <iostream>
 
-float pitchFilteredOld;
 Madgwick filter;
 const float sensorRate = 52.00;
-const float pitchSmooth = 0.3;
+const float pitchSmooth = 1.0;
+unsigned long microsPerReading, microsPrevious;
 
 //#include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 //char ssid[] = SECRET_SSID;        // your network SSID (name)
 //char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
-char ssid[] = "mokkula_482925";
-char pass[] = "HN7MEF63FJD";
+char* ssid = "mokkula_482925";
+char* pass = "HN7MEF63FJD";
 //char ssid[] = "crumbs";
 //char pass[] = "64795164";
 //char ssid[] = "uusikyla";
 //char pass[] = "painuhiiteen";
-char hostName[] = "192.168.8.100";
-int port = 9999;
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+char* host = "192.168.8.101";
+int port = 8000;
+const IPAddress ip(192, 168, 8, 150);
+const IPAddress gateway(192, 168, 8, 1);
+const IPAddress subnet(255, 255, 255, 0);
 
-WiFiClient client;
+//WiFiClient client;
 
 bool gotReply = false;
   
 float xAcc, yAcc, zAcc;
 float xGyro, yGyro, zGyro;
 float roll, pitch, yaw;
+float pitchFilteredOld;
 
 void setup() {
   Serial.begin(9600);
@@ -45,76 +49,57 @@ void setup() {
   }
 
   filter.begin(sensorRate);
-
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
+  
+  // initialize variables to pace updates to correct rate
+  microsPerReading = 1000000 / sensorRate;
+  microsPrevious = micros();
 
   // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(3000);
+  WiFi.begin(ssid, pass);
+  WiFi.config(ip, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
   }
+  Serial.print("WiFi connected, IP = ");
+  Serial.println(WiFi.localIP());
 
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network");
   printCurrentNet();
   printWifiData();
+  delay(2000);
 
-  while (!client.connected())
-  {
-    Serial.print("Connecting to server: ");
-    Serial.print(hostName);
-    Serial.print(":");
-    Serial.println(port);
-    if (client.connect(hostName, port)) {
-      Serial.println("connected");
-      Serial.print("Starting in 5 seconds...");
-      delay(5000);
-    }
-    else
-    {
-      Serial.println("Failed to connect to server. Retrying...");
-      delay(5000);
-    }
-  }
-
-  /*
-  while (true)
-  {
-    client.print(4343);
-    delay(3000);
-  }
-  */
+  OscWiFi.publish(host, port, "/publish/value", pitchFilteredOld, roll, yaw)
+        ->setFrameRate(sensorRate);
 }
 
 void loop() {
-  if(IMU.accelerationAvailable() && IMU.gyroscopeAvailable()){
-    IMU.readAcceleration(xAcc, yAcc, zAcc);
-    IMU.readGyroscope(xGyro, yGyro, zGyro); 
-    filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
-    pitch = filter.getPitch();
-    roll = filter.getRoll();
-    yaw = filter.getYaw();
-    float pitchFiltered = pitchSmooth * pitch + (1 - pitchSmooth) * pitchFilteredOld; // low pass filter
-    pitchFilteredOld = pitchFiltered;
-
-    Serial.println(pitchFiltered);
-
-    client.print('y');
-    client.print(pitchFiltered);
-    client.print('p');
-    client.print(roll);
-    client.print('r');
-    client.print(yaw);
+  unsigned long microsNow;
+  
+  //if(IMU.accelerationAvailable() && IMU.gyroscopeAvailable())
+  {
+    
+    // check if it's time to read data and update the filter
+    microsNow = micros();
+    
+    if (microsNow - microsPrevious >= microsPerReading) {
+      IMU.readAcceleration(xAcc, yAcc, zAcc);
+      IMU.readGyroscope(xGyro, yGyro, zGyro); 
+      filter.updateIMU(xGyro, yGyro, zGyro, xAcc, yAcc, zAcc);
+      pitch = filter.getPitch();
+      roll = filter.getRoll();
+      yaw = filter.getYaw();
+      float pitchFiltered = pitchSmooth * pitch + (1 - pitchSmooth) * pitchFilteredOld; // low pass filter
+      pitchFilteredOld = pitchFiltered;
+  
+      Serial.println(pitchFiltered);
+  
+      OscWiFi.post();
+      
+      // increment previous time, so we keep proper pace
+      microsPrevious = microsPrevious + microsPerReading;
+    }
   }
 
   //delay(15);  
