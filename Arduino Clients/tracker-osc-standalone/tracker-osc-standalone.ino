1ff077570d8bdcc566288e385faec1c04fc1ba8a
Tturna <eemelijuponaho@gmail.com>
2023-03-02 12:43:19 +0200

    standalone tracker, small doc start

--- Arduino Clients/tracker-osc-standalone/tracker-osc-standalone.ino
+++ Arduino Clients/tracker-osc-standalone/tracker-osc-standalone.ino
@@ -1,4 +1,9 @@
-#include <Arduino_LSM6DS3.h>
+// this tracker script will read gyro data and
+// send it to an OSC address via wifi.
+// This doesn't connect to a custom server like the previous
+// versions.
+
+include <Arduino_LSM6DS3.h>
 //#undef max
 //#undef min
 //#include <SPI.h>
@@ -40,11 +45,8 @@ float roll, pitch, yaw;
 float pitchFilteredOld;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
 
   if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
     while (1);
   }
 
@@ -58,16 +60,10 @@ void setup() {
   WiFi.begin(ssid, pass);
   WiFi.config(ip, gateway, subnet);
   while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
       delay(500);
   }
-  Serial.print("WiFi connected, IP = ");
-  Serial.println(WiFi.localIP());
 
   // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
   delay(2000);
 
   OscWiFi.publish(host, port, "/publish/value", pitchFilteredOld, roll, yaw)
@@ -93,8 +89,6 @@ void loop() {
       float pitchFiltered = pitchSmooth * pitch + (1 - pitchSmooth) * pitchFilteredOld; // low pass filter
       pitchFilteredOld = pitchFiltered;
   
-      Serial.println(pitchFiltered);
-  
       OscWiFi.post();
       
       // increment previous time, so we keep proper pace
@@ -103,54 +97,4 @@ void loop() {
   }
 
   //delay(15);  
-}
-
-void printWifiData() {
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println(ip);
-
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  printMacAddress(mac);
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
-void printMacAddress(byte mac[]) {
-  for (int i = 5; i >= 0; i--) {
-    if (mac[i] < 16) {
-      Serial.print("0");
-    }
-    Serial.print(mac[i], HEX);
-    if (i > 0) {
-      Serial.print(":");
-    }
-  }
-  Serial.println();
 }
